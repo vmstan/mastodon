93c49be0ca1bd6058828b55d0e6d41bd85c5e812
Michael Stanclift <mx@vmstan.com>
2023-09-29 19:34:33 -0500

    import max_toot if it exists

--- app/javascript/mastodon/initial_state.js
+++ app/javascript/mastodon/initial_state.js
@@ -147,6 +147,6 @@ export const criticalUpdatesPending = initialState?.critical_updates_pending;
 export const statusPageUrl = getMeta('status_page_url');
 export const sso_redirect = getMeta('sso_redirect');
 
-export const maxChars = (initialState && initialState.max_toot_chars) || 500;
+export const maxChars = initialState?.max_toot_chars || 500;
 
 export default initialState;
