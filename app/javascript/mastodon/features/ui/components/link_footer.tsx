557f497a427e457d1a97e31454926e678dd96dee
Michael Stanclift <mx@vmstan.com>
2026-01-05 14:39:14 -0600

    Make source_commit conditional
    Signed-off-by: Michael Stanclift <mx@vmstan.com>

--- app/javascript/mastodon/features/ui/components/link_footer.tsx
+++ app/javascript/mastodon/features/ui/components/link_footer.tsx
@@ -99,7 +99,8 @@ export const LinkFooter: React.FC<{
         </a>
         <DividingCircle />
         <span className='version'>
-          v{version} ({source_commit})
+          v{version}
+          {source_commit && ` (${source_commit})`}
         </span>
       </p>
     </div>
